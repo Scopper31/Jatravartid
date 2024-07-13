84efe67d17402f16a8cffac2cec7f3732fe8fe75
Scopper31 <84668306+Scopper31@users.noreply.github.com>
2024-07-13 14:47:29 +0300

    Update main.py

--- Jatravartid/main.py
+++ Jatravartid/main.py
@@ -12,7 +12,7 @@ import ast
 import traceback
 
 
-API_KEY = "AIzaSyB-o46jLBbVAmdl--6OJq6WEHRsDQJlUH0"
+API_KEY = ""
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
