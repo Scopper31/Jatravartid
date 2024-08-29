8175fbad93591a264c9bed1a07e2d9572b882cd2
svbudygin <svbudygin@yandex.ru>
2024-08-29 09:04:06 +0300

    fix: major restructure

--- code/config.py
+++ code/config.py
@@ -2,9 +2,9 @@ import google.generativeai as genai
 import os
 
 # gemini конфигурация
-# API_KEY = "AIzaSyDLHHsHyTOHOUkErElPpnvbPsJ7dcu2BKc"
-API_KEY = "AIzaSyBM6bOXdvcdacg-Bo9SavNY7WqTnqI4Zzw"
-# AIzaSyBM6bOXdvcdacg-Bo9SavNY7WqTnqI4Zzw
+# API_KEY = "AIzaSyDLHHsHyTOHOUkErElPpnvbPsJ7dcu2BKc" #alex
+API_KEY = "AIzaSyBM6bOXdvcdacg-Bo9SavNY7WqTnqI4Zzw" #svb
+"AIzaSyCOtj2v7Thm2GH7liup-wH_35OgmNAg2jU" # М
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
